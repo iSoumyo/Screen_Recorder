ef7b8577193d5353df87633c9aca0d7ba9cc4c17
Soumyojyoti Sen <82030266+iSoumyo@users.noreply.github.com>
2021-06-15 21:23:36 +0530

    Update Screen_Recorder.py

--- Screen_Recorder.py
+++ Screen_Recorder.py
@@ -11,12 +11,12 @@ time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
 file_name = f'{time_stamp}.mp4'
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 captured_video = cv2.VideoWriter(file_name, fourcc, 20.0, (width, height))
-webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 while True:
     img = ImageGrab.grab(bbox=(0, 0, width, height))
     img_np = np.array(img)
     img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
-    _, frame = webcam.read()
+    _, frame = cam.read()
     fr_height, fr_width, _ = frame.shape
     img_final[0:fr_height, 0: fr_width, :] = frame[0: fr_height, 0: fr_width, :]
     cv2.imshow('Secret Capture', img_final)
